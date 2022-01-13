2dd5977ea97e69b9e64f21a58f9c8c87e97a8945
Astrospence <illustrator.wood@gmail.com>
2022-01-12 22:22:11 -0700

    wrote get comments of post enpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -78,4 +78,18 @@ router.delete('/:id', (req, res) => {
     })
 })
 
+router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+    .then(resp => {
+        if (!resp) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist' })
+        } else {
+            res.status(200).json(resp)
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'The comments information could not be retrieved' })
+    })
+})
+
 module.exports = router
