ffe5dc0cddb4c2586353047a0ae71bcb4505ff8e
Astrospence <illustrator.wood@gmail.com>
2022-01-12 20:23:54 -0700

    initialized posts-router.js and wrote get all endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,22 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(resp => {
+        if (!resp) {
+            res.status(500).json({ message: 'The posts information could not be retrieved'})
+        } else {
+            res.status(200).json(resp)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: err.message})
+    })
+})
+
+
+
+module.exports = router
