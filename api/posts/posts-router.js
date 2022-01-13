453232201fb385a819f053b1400f616205955db8
Astrospence <illustrator.wood@gmail.com>
2022-01-12 20:54:51 -0700

    wrote get by id endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -17,6 +17,18 @@ router.get('/', (req, res) => {
     })
 })
 
-
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(resp => {
+        if (!resp || resp === null) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist'})
+        } else {
+            res.status(200).json(resp)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: err.message})
+    })
+})
 
 module.exports = router
